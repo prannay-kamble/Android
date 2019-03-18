8f78598858c7e779beb2a929a0fe2a4a2d222bef
Pranay Kamble <pranay.kamble@synapse.com>
2019-03-18 11:03:37 +0530

    location app commit

---
+++ app/src/main/java/com/gl/localizationapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.gl.localizationapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView1;
+    EditText username_editText;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.ACCESSIBILITY_SERVICE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.relativeId);
+        layout.addView(textView);
+
+    }
+}
